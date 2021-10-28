c9d7a2953e92ed233a3fe53da1f2ad7e3127ca9b
bianchui <bianchui@gmail.com>
2021-10-28 12:56:20 +0800

    1.1.8

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 readonly THIS_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"
 
 pushd $THIS_DIR/vendor/toc/js
-uglifyjs ztree_toc.js -o ztree_toc.min.js -c pure_funcs="console.log" -m keep_fnames=false --mangle-props regex=/^_/
+uglifyjs ztree_toc.js -o ztree_toc.min.js -c toplevel,pure_funcs="_log",drop_console -m keep_fnames=false,toplevel,reserved=[],properties --mangle-props regex=/^_/
